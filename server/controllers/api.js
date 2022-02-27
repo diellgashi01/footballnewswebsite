b1b500b33573f6183c2f71f48075bc95cb38f8c1
Diell Gashi <dg47881@ubt-uni.net>
2022-02-27 14:49:03 +0100

    Created create method

--- server/controllers/api.js
+++ server/controllers/api.js
@@ -18,7 +18,15 @@ module.exports = class API {
 
     //create a post
     static async createPost(req, res){
-        res.send("Create Post");
+        const post = req.body;
+        const imagename = req.file.filename;
+        post.image = imagename;
+        try{
+            await Post.create(post);
+            res.status(201).json({message: 'Post created successfully!'});
+        } catch(err){
+            res.status(400).json({message: err.message});
+        }
     }
 
     //update a post
