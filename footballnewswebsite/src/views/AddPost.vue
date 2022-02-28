61d70d2545eb151345bd5c814b19aaace9f1befa
Nazmi Cakiqi <nc47809@ubt-uni.net>
2022-02-28 17:19:29 +0100

    Add more features to AddPost page

--- footballnewswebsite/src/views/AddPost.vue
+++ footballnewswebsite/src/views/AddPost.vue
@@ -5,11 +5,11 @@
                 <v-card class="pa-5">
                     <v-card-title>Add New Post</v-card-title>
                     <v-divider></v-divider>
-                    <v-form ref="form" class="pa-5" enctype="multipart/form-data">
-                        <v-text-field label="Title" prepend-icon="mdi-note" :rules="rules"></v-text-field>
-                        <v-text-field label="Category" prepend-icon="mdi-view-list" :rules="rules"></v-text-field>
-                        <v-textarea label="Content" prepend-icon="mdi-note-plus" :rules="rules"></v-textarea>
-                        <v-file-input :rules="rules" show-size counter multiple label="Select Image"></v-file-input>
+                    <v-form ref="form" @submit.prevent="submitForm" class="pa-5" enctype="multipart/form-data">
+                        <v-text-field label="Title" v-model="post.title" prepend-icon="mdi-note" :rules="rules"></v-text-field>
+                        <v-text-field label="Category" v-model="post.category" prepend-icon="mdi-view-list" :rules="rules"></v-text-field>
+                        <v-textarea label="Content" v-model="post.content" prepend-icon="mdi-note-plus" :rules="rules"></v-textarea>
+                        <v-file-input @change="selectFile" :rules="rules" show-size counter multiple label="Select Image"></v-file-input>
                         <v-btn type="submit" class="mt-3" color="primary">Add Post</v-btn>
                     </v-form> 
                 </v-card>
@@ -17,3 +17,36 @@
         </v-row>
     </v-container>
 </template>
+<script>
+import API from "../api";
+export default {
+    data(){
+        return{
+            rules: [(value)=>!!value || "This field is required!"],
+            post: {
+               title: "",
+               category: "",
+               content: "",
+               image: ""
+            },
+            image: "",
+        };
+    },
+    methods: {
+        selectFile(file){
+            this.image - file[0];
+        },
+        async submitForm(){
+            const fromData = new FormData();
+            fromData.append('image', this.image);
+            fromData.append('title', this.post.title);
+            fromData.append('category', this.post.category);
+            fromData.append('content', this.post.content);
+            if(this.$refs.form.validate()){
+                const response = await API.addPost(formData);
+                this.$router.push({ name: 'home', params: {message: response.message}});
+            }
+        },
+    },
+};
+</script>
