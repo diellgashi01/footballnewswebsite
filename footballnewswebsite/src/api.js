1a23ad854c93048199835fea29810315a5428166
Nazmi Cakiqi <nc47809@ubt-uni.net>
2022-02-28 15:49:52 +0100

    Create news article

--- footballnewswebsite/src/api.js
+++ footballnewswebsite/src/api.js
@@ -14,7 +14,17 @@ export default class API {
     }
     //to insert post into database 
     static async addPost(post){
-        const res = await axios.get(url, post);
+        const res = await axios.post(url, post);
+        return res.data;
+    }
+    //to uptade post into database 
+    static async addPost(id, post){
+        const res = await axios.patch(`${url}/${id}`, post);
+        return res.data;
+    }
+    //to delete a post
+    static async deletePost(id){
+        const res = await axios.delete(`${url}/${id}`);
         return res.data;
     }
 }
--- footballnewswebsite/src/views/HomeView.vue
+++ footballnewswebsite/src/views/HomeView.vue
@@ -1,12 +1,36 @@
 <template>
- <h1>Home page</h1>
+  <v-container>
+    <v-row>
+      <v-col sm="4" class="pa-3" v-for="post in posts" :key="post._id">
+        <v-card class="pa-1">
+          <v-img height="250" :src="`/${post.image}`"></v-img>
+          <v-btn class="m1-4 mt-3" small outlined color="indigo">
+            {{ post.category }}
+          </v-btn>
+          <v-card-title class="headline">
+            {{post.title}}
+          </v-card-title>
+          <v-card-text class="py-0">
+            <p>{{post.content.substring(0, 100) + "..."}}</p>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
 
-
+import API from "../api";
 export default {
   name: 'HomeView',
-
+  data(){
+    return{
+      posts: [],
+    }
+  },
+  async created(){
+    this.posts = await API.getAllPost();
+  },
 }
 </script>
